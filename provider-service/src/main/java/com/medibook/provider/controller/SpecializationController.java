c74311ef591be92110448c8088d052065c9ae054
Himanshu Patel <patelhimanshu609@gmail.com>
2026-05-16 12:35:47 +0530

    [Himanshu Patel] refactor updating provider-service

--- provider-service/src/main/java/com/medibook/provider/controller/SpecializationController.java
+++ provider-service/src/main/java/com/medibook/provider/controller/SpecializationController.java
@@ -1,12 +1,21 @@
 package com.medibook.provider.controller;
 
-import com.medibook.provider.entity.Specialization;
-import com.medibook.provider.repository.SpecializationRepository;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.medibook.provider.entity.Specialization;
+import com.medibook.provider.repository.SpecializationRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/specializations")
